f846c5a6cd35bc05a98fb5788dc8d62bad9e9bab
eshaffer321 <erick.shaffer321@gmail.com>
2025-06-09 09:22:22 -0600

    Improve options UI styling

--- src/options/OptionsPage.svelte
+++ src/options/OptionsPage.svelte
@@ -2,6 +2,6 @@
 import InfoPanel from './components/InfoPanel.svelte';
 </script>
 
-<div id="options-container" class="min-h-screen bg-gray-100 p-6">
+<div id="options-container" class="min-h-screen bg-gradient-to-br from-indigo-50 to-white p-6 font-sans text-gray-700">
     <InfoPanel />
 </div>
