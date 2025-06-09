5200493fe05dc93c7dfc7901dd718f1456d7e200
eshaffer321 <erick.shaffer321@gmail.com>
2025-06-09 07:06:53 -0600

    Fix build warnings

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,21 @@
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 import { string } from 'rollup-plugin-string';
 import svelte from 'rollup-plugin-svelte';
 
+function onwarn(warning, warn) {
+       if (warning.code === 'CIRCULAR_DEPENDENCY' && /svelte/.test(warning.message)) {
+               return;
+       }
+       warn(warning);
+}
+
 const plugins = [
        svelte({ emitCss: true }),
-       resolve(),
+       resolve({ exportConditions: ['svelte'] }),
        commonjs(),
        typescript(),
 	replace({
@@ -21,33 +28,36 @@ const plugins = [
 ];
 
 const intercept = {
-	input: 'src/intercept.ts',
-	output: {
-		file: 'build/intercept.js',
-		format: 'iife',
-		name: 'Intercept',
-	},
-	plugins: [...plugins, css({ exclude: '**/*.str.css', output: 'build/eradicate.css' })],
+        input: 'src/intercept.ts',
+        output: {
+                file: 'build/intercept.js',
+                format: 'iife',
+                name: 'Intercept',
+        },
+       plugins: [...plugins, css({ exclude: '**/*.str.css', output: 'build/eradicate.css' })],
+       onwarn,
 };
 
 const options = {
-	input: 'src/options/options.ts',
-	output: {
-		file: 'build/options.js',
-		format: 'iife',
-		name: 'Options',
-	},
-	plugins: [...plugins, css({ exclude: '**/*.str.css', output: 'build/options.css' })],
+        input: 'src/options/options.ts',
+        output: {
+                file: 'build/options.js',
+                format: 'iife',
+                name: 'Options',
+        },
+       plugins: [...plugins, css({ exclude: '**/*.str.css', output: 'build/options.css' })],
+       onwarn,
 };
 
 const background = {
-	input: 'src/background/service-worker.ts',
-	output: {
-		file: 'build/service-worker.js',
-		format: 'iife',
-		name: 'ServiceWorker',
-	},
-	plugins,
+        input: 'src/background/service-worker.ts',
+        output: {
+                file: 'build/service-worker.js',
+                format: 'iife',
+                name: 'ServiceWorker',
+        },
+       plugins,
+       onwarn,
 };
 
 export default [intercept, options, background];
