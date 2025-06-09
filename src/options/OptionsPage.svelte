71e5aa0bec3bb725e290f6bcec0a42df48dd8b84
eshaffer321 <erick.shaffer321@gmail.com>
2025-06-09 10:30:16 -0600

    Restyle options layout with Tailwind containers

--- src/options/OptionsPage.svelte
+++ src/options/OptionsPage.svelte
@@ -2,6 +2,8 @@
 import InfoPanel from './components/InfoPanel.svelte';
 </script>
 
-<div id="options-container" class="min-h-screen bg-gradient-to-br from-indigo-50 to-white p-6 font-sans text-gray-700">
-    <InfoPanel />
+<div id="options-container" class="min-h-screen p-6">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <InfoPanel />
+    </div>
 </div>
