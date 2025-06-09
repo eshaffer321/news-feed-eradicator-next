990c5583440f7767aabfe37b26b58c3d5ecbe75a
eshaffer321 <erick.shaffer321@gmail.com>
2025-06-09 08:34:09 -0600

    feat(options): migrate UI to Svelte

---
+++ src/options/components/QuoteOptions.svelte
@@ -0,0 +1,26 @@
+<script>
+import { BackgroundActionType } from '../../background/store/action-types';
+import { ActionType } from '../../store/action-types';
+export let store;
+
+function toggleQuotes() {
+    store.dispatch({ type: ActionType.BACKGROUND_ACTION, action: { type: BackgroundActionType.QUOTES_SHOW_TOGGLE } });
+}
+function toggleBuiltin() {
+    store.dispatch({ type: ActionType.BACKGROUND_ACTION, action: { type: BackgroundActionType.QUOTES_BUILTIN_TOGGLE } });
+}
+</script>
+
+<div class="v-stack-2">
+    <h2>Quotes</h2>
+    <label>
+        <input type="checkbox" checked={store.getState().settings?.showQuotes} on:change={toggleQuotes} />
+        Show Quotes
+    </label>
+    {#if store.getState().settings?.showQuotes}
+        <label>
+            <input type="checkbox" checked={store.getState().settings?.builtinQuotesEnabled} on:change={toggleBuiltin} />
+            Enable Built-in Quotes
+        </label>
+    {/if}
+</div>
