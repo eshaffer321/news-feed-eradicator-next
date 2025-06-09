60a0846ed1541efb2b290a481db09ca5e7031947
eshaffer321 <erick.shaffer321@gmail.com>
2025-06-09 08:16:48 -0600

    Restore options UI via Snabbdom

--- src/options/OptionsPage.svelte
+++ src/options/OptionsPage.svelte
@@ -1,7 +1,42 @@
 <script>
-// Svelte component for options page
+import { onMount } from 'svelte';
+import { createStore } from '../store/index';
+import InfoPanel from '../components/info-panel';
+import { init } from 'snabbdom';
+import propsModule from 'snabbdom/modules/props';
+import attrsModule from 'snabbdom/modules/attributes';
+import eventsModule from 'snabbdom/modules/eventlisteners';
+import { toVNode } from 'snabbdom/tovnode';
+import { ActionType } from '../store/action-types';
+import { BackgroundActionType } from '../background/store/action-types';
+import { SECOND } from '../lib/time';
+
+let container;
+
+onMount(() => {
+        const store = createStore();
+        const patch = init([propsModule, attrsModule, eventsModule]);
+        let vnode = toVNode(container);
+
+        store.dispatch({
+                type: ActionType.BACKGROUND_ACTION,
+                action: { type: BackgroundActionType.FEATURE_INCREMENT },
+        });
+
+        const render = () => {
+                const newVnode = InfoPanel(store);
+                patch(vnode, newVnode);
+                vnode = newVnode;
+        };
+        store.subscribe(render);
+
+        setInterval(() => {
+                const state = store.getState();
+                if (state.uiOptions.tab === 'sites') render();
+        }, 30 * SECOND);
+
+        render();
+});
 </script>
 
-<div class="nfe-info-panel">
-<h3 class="text-center">News Feed Eradicator</h3>
-</div>
+<div bind:this={container} id="options-container"></div>
